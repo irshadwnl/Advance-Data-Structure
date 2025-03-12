513f266711a76c9fe9898905e4161335effbcf96
irshadwnl <irshadwnl07@gmail.com>
2025-03-12 14:20:59 +0530

    added linkedlist implementation

---
+++ LinkedList/Implementation.java
@@ -0,0 +1,21 @@
+package LinkedList
+
+
+class Node{
+    int data;
+    Node next;
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+public class Implementation {
+    public static void main(String[] args) {
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+
+    }
+    
+}
