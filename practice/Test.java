c35f61875413cc632e837dde1d31df322bd3c90d
irshadwnl <irshadwnl07@gmail.com>
2025-03-19 10:26:34 +0530

    added practice folders

---
+++ practice/Test.java
@@ -0,0 +1,10 @@
+package practice;
+public class Test {
+    public static void main(String[] args) {
+        int arr[]={1,2,34};
+        for(int i=0;i<3;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
